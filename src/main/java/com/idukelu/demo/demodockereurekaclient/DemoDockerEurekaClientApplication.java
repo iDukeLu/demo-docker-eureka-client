50d53eeabd8014e9c4c3d746bf4d05873edeaf03
Duke Lu <23153285987@qq.com>
2019-01-31 20:56:50 +0800

    初始化项目

---
+++ src/main/java/com/idukelu/demo/demodockereurekaclient/DemoDockerEurekaClientApplication.java
@@ -0,0 +1,14 @@
+package com.idukelu.demo.demodockereurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDockerEurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDockerEurekaClientApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/idukelu/demo/demodockereurekaclient/DemoDockerEurekaClientApplicationTests.java
@@ -0,0 +1,17 @@
+package com.idukelu.demo.demodockereurekaclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoDockerEurekaClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
