06c7d2bd1a696b41867163d38d029293cb0f60b9
Duke Lu <23153285987@qq.com>
2019-02-17 20:43:50 +0800

    修改主类名称

--- src/main/java/com/idukelu/demo/demodockereurekaclient/Application.java
+++ src/main/java/com/idukelu/demo/demodockereurekaclient/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoDockerEurekaClientApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoDockerEurekaClientApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
